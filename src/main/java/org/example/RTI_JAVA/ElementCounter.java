de6a0226d7fffc4d37d74f3e5b0346de40451cc9
АНС <atanovnikitatlt@gmail.com>
2024-01-08 21:34:05 +0300

    JAVA and groovy

--- src/main/java/org/example/RTI_JAVA/ElementCounter.java
+++ src/main/java/org/example/RTI_JAVA/ElementCounter.java
@@ -1,2 +1,21 @@
-package org.example.RTI_JAVA;public class ElementCounter {
+package org.example.RTI_JAVA;
+import java.util.*;
+public class ElementCounter {
+    public static void counter() {
+
+        int[] arr = {1, 3, 4, 5, 1, 5, 4};
+
+        Map<Integer, Integer> elementCount = new HashMap<>();
+
+        for (int element : arr) {
+            elementCount.put(element, elementCount.getOrDefault(element, 0) + 1);
+        }
+
+        System.out.println(elementCount);
+    }
+
+
+
+
+
 }
--- src/main/java/org/example/RTI_groovy/ElementCounterGroovy.groovy
+++ src/main/java/org/example/RTI_groovy/ElementCounterGroovy.groovy
@@ -1,4 +1,25 @@
 package org.example.RTI_groovy
 
 class ElementCounterGroovy {
+
+
+    static def countElements(list) {
+        def map = [:]
+        list.each {
+            if (map.containsKey(it)) {
+                map[it]++
+            } else {
+                map[it] = 1
+            }
+        }
+        return map
+    }
+
+    def list = [1, 3, 4, 5, 1, 5, 4]
+    public def result = countElements(list)
+    println result
+
+
+
+
 }
