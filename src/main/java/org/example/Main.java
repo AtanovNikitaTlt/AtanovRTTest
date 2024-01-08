3c1ae7e6c6a2a07c0e8628a33b61ef2c72fafac6
АНС <atanovnikitatlt@gmail.com>
2024-01-08 21:34:05 +0300

    JAVA and groovy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
+import org.example.RTI_JAVA.ElementCounter;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 
 public class Main {
     public static void main(String[] args) {
+        Binding binding = new Binding();
+        GroovyShell shell = new GroovyShell(binding);
+
+        String scriptPath = "org/example/RTI_groovy/ElementCounterGroovy.groovy";
+        try {
+            Object result = shell.run(scriptPath, "ElementCountGroovy", (String[]) new Object[]{new Integer[]{1, 3, 4, 5, 1, 5, 4}});
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
         System.out.println("Hello world!");
+        ElementCounter.counter();
+
+
     }
 }
--- src/test/java/org/example/RTU_JAVA/ElementCounterTest.java
+++ src/test/java/org/example/RTU_JAVA/ElementCounterTest.java
@@ -1,2 +1,30 @@
-package org.example.RTU_JAVA;public class ElementCounterTest {
+package org.example.RTU_JAVA;
+
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ElementCounterTest {
+    @Test
+    public void testElementCounter() {
+        int[] arr = {1, 3, 4, 5, 1, 5, 4};
+
+        Map<Integer, Integer> expected = new HashMap<>();
+        expected.put(1, 2);
+        expected.put(3, 1);
+        expected.put(4, 2);
+        expected.put(5, 2);
+
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int element : arr) {
+            result.put(element, result.getOrDefault(element, 0) + 1);
+        }
+
+        assertEquals(expected, result);
+    }
+
 }
